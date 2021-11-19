77eb7b03fe9e24824768fc9fffea222514c78129
anugrahkresnaya <anugrahkresnaya@gmail.com>
2021-11-19 19:38:31 +0700

    make a message

---
+++ app.js
@@ -0,0 +1,16 @@
+const chara = {
+    'advenJob' : ['Warrior', 'Dragon Knight', 'Barbarian', 'Monk', 'Ranger', 'Assassin', 'Thief', 'Rogue', 'Sorcerer', 'Warden', 'Necromancer'],
+    'weapon' : ['Sword', 'Claymore', 'Wand', 'Fist', 'Bow', 'Dual Dagger'],
+    'magicElem' : ['Fire', 'Dark', 'Light', 'Heal', 'Ice', 'Lightning', 'Earth'],
+    'passiveSkill' : ['Language Knowledge', 'Regeneration', 'Instant Learning', 'Luck', 'Enhanced Awareness', 'Enticing Charm', 'Fast Increase Growth', 'Survivalist'],
+    'activeSkill' : ['Omniscient', 'Appraisal', 'Duplicate', 'Teleportation', 'Skill Creation'],
+    'uniqueSkill' : ['Arise'],
+    'stats' : {
+        'Str' : undefined,
+        'Def' : undefined,
+        'Agi' : undefined,
+        'int' : undefined,
+        'Dex' : undefined,
+        'Vit' : undefined
+    }
+}
